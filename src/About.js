92db2f0cc569af08fa98a9bf655a165d745b0a06
Timothy Williams <113715036+shwifty-man@users.noreply.github.com>
2025-08-10 14:29:00 -0400

    create about section shwoing what you can do

--- src/About.js
+++ src/About.js
@@ -1,10 +1,60 @@
 
 function About() {
-    return <section className="About-container">
-        <div className="About-container-header">
-            <h2>Things <span className="Main-body-im">I</span> can do</h2>
-        </div>
-    </section>
- }
+  return (
+    <section className="About-container">
+      <div className="About-container-header">
+        <h3>
+          My <span className="Main-body-im">services</span>
+        </h3>
+        <h2>What I do</h2>
+      </div>
+      <div className="About-container-body">
+        <div className="About-container-square-div">
+          <div className="About-container-square-boxes">
+            <i
+              className="fas fa-desktop"
+              style={{ fontSize: "48px", color: "red" }}
+            ></i>
 
- export default About
+            <h3 className="inner-box-h3">Lorem ipsum</h3>
+            <div className="inner-box-text">
+              Lorem ipsum is a dummy or placeholder text commonly
+            </div>
+          </div>
+          <div className="About-container-square-boxes">
+            <i
+              className="fas fa-desktop"
+              style={{ fontSize: "48px", color: "red" }}
+            ></i>
+            <h3 className="inner-box-h3">Lorem ipsum</h3>
+            <div className="inner-box-text">
+              Lorem ipsum is a dummy or placeholder text commonly
+            </div>
+          </div>
+          <div className="About-container-square-boxes">
+            <i
+              className="fas fa-desktop"
+              style={{ fontSize: "48px", color: "red" }}
+            ></i>
+            <h3 className="inner-box-h3">Lorem ipsum</h3>
+            <div className="inner-box-text">
+              Lorem ipsum is a dummy or placeholder text commonly
+            </div>
+          </div>
+          <div className="About-container-square-boxes">
+            <i
+              className="fas fa-desktop"
+              style={{ fontSize: "48px", color: "red" }}
+            ></i>
+            <h3 className="inner-box-h3">Lorem ipsum</h3>
+            <div className="inner-box-text">
+              Lorem ipsum is a dummy or placeholder text commonly
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
