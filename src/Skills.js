fd2db86f8b75185772f72a0ae79d28d7fa93917b
Timothy Williams <113715036+shwifty-man@users.noreply.github.com>
2025-08-11 17:51:06 -0400

    create skills section and add icons

--- src/Skills.js
+++ src/Skills.js
@@ -1,6 +1,37 @@
-
 function Skills() {
-    return <section className="Skilles-container"></section>
- }
+  return (
+    <section className="Skills-container">
+      <div className="Skills-inner-container">
+        <div className="Skills-card">
+          <i class="devicon-html5-plain-wordmark colored"></i>
+        </div>
+        <div className="Skills-card">
+          <i class="devicon-css3-plain-wordmark colored"></i>
+        </div>
+        <div className="Skills-card">
+          <i class="devicon-javascript-plain colored"></i>
+        </div>
+        <div className="Skills-card">
+          <i class="devicon-react-original-wordmark colored"></i>
+        </div>
+        <div className="Skills-card">
+          <i class="devicon-nodejs-plain-wordmark colored"></i>
+        </div>
+        <div className="Skills-card">
+          <i class="devicon-bash-plain "></i>
+        </div>
+        <div className="Skills-card">
+          <i class="devicon-linux-plain colored"></i>
+        </div>
+        <div className="Skills-card">
+          <i class="devicon-windows11-original-wordmark colored"></i>
+        </div>
+        <div className="Skills-card">
+          <i class="devicon-express-original-wordmark"></i>
+        </div>
+      </div>
+    </section>
+  )
+}
 
- export default Skills
+export default Skills
