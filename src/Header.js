fc7f816b43515997b4d41b089d56561f78cbeecb
Timothy Williams <113715036+shwifty-man@users.noreply.github.com>
2025-08-11 19:11:10 -0400

    add text and style the header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,26 @@
- function Header() {
-    return (
-      <header className="Header">
-        <div className="Header-title">Timothy</div>
-        <div className="Header-a-Home"></div>
-        <div className="Header-a-Projects"></div>
-        <div className="Header-a-Contact"></div>
-      </header>
-    )
- }
+function Header() {
+  return (
+    <header className="Header">
+      <div className="Header-title">
+        <h2 className="Header-h2-name">Timothy Williams</h2>
+      </div>
+      <div>
+        <h2 className="Header-a">
+          <a href="/Main-body">Home</a>
+        </h2>
+      </div>
+      <div>
+        <h2 className="Header-a">
+          <a>Projects</a>
+        </h2>
+      </div>
+      <div>
+        <h2 className="Header-a Header-contact">
+          <a>Contact</a>
+        </h2>
+      </div>
+    </header>
+  )
+}
 
- export default Header
+export default Header
