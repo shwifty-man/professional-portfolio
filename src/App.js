ce2fece324843a2237541dbf7034a89408a35619
Timothy Williams <113715036+shwifty-man@users.noreply.github.com>
2025-08-11 17:54:26 -0400

    add particle background

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './Skills.css';
 import './Projects.css';
 import './Contact.css';
 import './Footer.css';
+import './App.scss';
 import Header from './Header.js';
 import Home from './Home.js';
 import About from'./About.js';
@@ -17,16 +18,27 @@ import Footer from'./Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      <div className="page-background">
+        <div className="animation-wrapper">
+          <div className="particle particle-1"></div>
+          <div className="particle particle-2"></div>
+          <div className="particle particle-3"></div>
+          <div className="particle particle-4"></div>
+        </div>
+      </div>
+
+      <div className="App">
+        <Header />
+        <Home />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </>
   )
 }
 
-export default App;
+export default App
