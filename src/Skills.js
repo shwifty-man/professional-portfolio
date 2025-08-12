528d4c1f7b1b92811205a5f420c8e242291fe871
Timothy Williams <113715036+shwifty-man@users.noreply.github.com>
2025-08-12 17:01:28 -0400

    change stlying to skills

--- src/Skills.js
+++ src/Skills.js
@@ -1,36 +1,39 @@
 function Skills() {
   return (
-    <section className="Skills-container">
-      <div className="Skills-inner-container">
-        <div className="Skills-card">
-          <i class="devicon-html5-plain-wordmark colored"></i>
+    <>
+    <h2 id="Skills-h2">Skills</h2>
+      <section className="Skills-container">
+        <div className="Skills-inner-container">
+          <div className="Skills-card">
+            <i class="devicon-html5-plain-wordmark colored"></i>
+          </div>
+          <div className="Skills-card">
+            <i class="devicon-css3-plain-wordmark colored"></i>
+          </div>
+          <div className="Skills-card">
+            <i class="devicon-javascript-plain colored"></i>
+          </div>
+          <div className="Skills-card">
+            <i class="devicon-react-original-wordmark colored"></i>
+          </div>
+          <div className="Skills-card">
+            <i class="devicon-nodejs-plain-wordmark colored"></i>
+          </div>
+          <div className="Skills-card">
+            <i class="devicon-bash-plain "></i>
+          </div>
+          <div className="Skills-card">
+            <i class="devicon-linux-plain colored"></i>
+          </div>
+          <div className="Skills-card">
+            <i class="devicon-windows11-original-wordmark colored"></i>
+          </div>
+          <div className="Skills-card">
+            <i class="devicon-express-original-wordmark"></i>
+          </div>
         </div>
-        <div className="Skills-card">
-          <i class="devicon-css3-plain-wordmark colored"></i>
-        </div>
-        <div className="Skills-card">
-          <i class="devicon-javascript-plain colored"></i>
-        </div>
-        <div className="Skills-card">
-          <i class="devicon-react-original-wordmark colored"></i>
-        </div>
-        <div className="Skills-card">
-          <i class="devicon-nodejs-plain-wordmark colored"></i>
-        </div>
-        <div className="Skills-card">
-          <i class="devicon-bash-plain "></i>
-        </div>
-        <div className="Skills-card">
-          <i class="devicon-linux-plain colored"></i>
-        </div>
-        <div className="Skills-card">
-          <i class="devicon-windows11-original-wordmark colored"></i>
-        </div>
-        <div className="Skills-card">
-          <i class="devicon-express-original-wordmark"></i>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
 
