9f595618cc436d4726bffbbcb6599bd9af88e623
Timothy Williams <113715036+shwifty-man@users.noreply.github.com>
2025-08-21 17:07:39 -0400

    change projects to coming soon and add onClick ability (to example.com temporarily)

--- src/Projects.js
+++ src/Projects.js
@@ -1,45 +1,43 @@
 function Projects() {
   return (
     <>
-      <h2 id="Projects-h2">
-        My Work <span className="Main-body-im h2-arrow">↓</span>
-      </h2>
+      <h2 id="Projects-h2">My Work</h2>
       <section className="Projects-container">
         <div className="Projects-top-container Project-cards">
           <div className="project-box top-project-box">
-            <h2>Project Name</h2>
+            <h2>Coming soon!</h2>
             <div className="img-div">
               <img
                 className="project-image"
                 src="/IMGs/image.png"
                 alt="project screenshot"
+                onClick={() => window.open("https://example.com/", "_blank")}
               />
             </div>
           </div>
           <div className="project-text">
             <p className="project-p">
-              Lorem ipsum is a dummy or placeholder text commonly used in
-              graphic design, publishing, and web development. Its purpose is to
-              permit a page layout to be designed
+              New projects are currently in development and will be featured
+              here soon. Please check back later to explore my latest work.
             </p>
           </div>
         </div>
         <div className="Projects-bottom-container Project-cards">
           <div className="project-box bottom-project-box">
-            <h2>Project Name</h2>
+            <h2>Coming soon!</h2>
             <div className="img-div">
               <img
                 className="project-image"
                 src="/IMGs/image.png"
                 alt="project screenshot"
+                onClick={() => window.open("https://example.com/", "_blank")}
               />
             </div>
           </div>
           <div className="project-text bottom-project-text">
             <p className="project-p">
-              Lorem ipsum is a dummy or placeholder text commonly used in
-              graphic design, publishing, and web development. Its purpose is to
-              permit a page layout to be designed
+              New projects are currently in development and will be featured
+              here soon. Please check back later to explore my latest work.
             </p>
           </div>
         </div>
